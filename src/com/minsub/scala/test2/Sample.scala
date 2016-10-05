2de6a499dc4866320101bcce3d937b27daa70362
jiminsub <jiminsub@gmail.com>
2016-10-05 10:33:44 +0900

    Sample upload

---
+++ src/com/minsub/scala/test2/Sample.scala
@@ -0,0 +1,10 @@
+package com.minsub.scala.test2
+
+/**
+  * Created by hmm1115222 on 2016-10-05.
+  */
+object Sample {
+  def main(args: Array[String]): Unit = {
+    println("Sample TEST!!")
+  }
+}
